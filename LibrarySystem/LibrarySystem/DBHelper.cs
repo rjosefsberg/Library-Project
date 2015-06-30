66274cac5d691f1d32e812aa473534759a7e08f8
rjosefsberg <rjosefsberg@gmail.com>
2015-06-30 14:46:18 -0400

    Library System Project
    Added the project file, which contains the Main and DBHelper class.
    DBHelper class is still under revision.

---
+++ LibrarySystem/LibrarySystem/DBHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+public class DBHelper
+{
+    private String cs = "Server = localhost; Database=mydb;Uid = root;password=root";
+       
+    public void dbConnect()
+    {
+         try
+         {
+             String cs = "server=localhost;database=mydb;uid=root;password=root";
+             MySqlConnection connect = new MySqlConnection(cs);
+             connect.Open();
+             MySqlCommand myCommand = new MySqlCommand();
+             myCommand.Connection = connect;
+             myCommand.CommandText = "select * from department";
+
+             MySqlDataReader myReader = myCommand.ExecuteReader();
+               
+              while (myReader.Read())
+              {
+                    Console.WriteLine(myReader["DNAME"].ToString());
+                    Console.WriteLine(myReader["DNUMBER"].ToString());
+                    Console.WriteLine(myReader["MGRSSN"].ToString());
+                    Console.WriteLine(myReader["MGRSTARTDATE"].ToString());
+              }
+              Console.ReadLine();
+              connect.Close();
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+}
+
+
+
---
+++ LibrarySystem/LibrarySystem/main.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data;
+
+public class LibrarySystem
+{
+    public static void Main(string[] args)
+    {
+        DBHelper db = new DBHelper();
+        db.dbConnect();
+    }
+}
